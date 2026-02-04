330df34e4c94497a3039ffb0e2b03b5359e8eda3
Tan Chun Siong <tanchunsiong@outlook.sg>
2026-02-04 12:02:58 +0800

    Fix syntax error in console.log

--- index.js
+++ index.js
@@ -339,5 +339,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(\`Server running on port \${PORT}\`);
+  console.log('Server running on port ' + PORT);
 });
